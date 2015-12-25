c32e6d7689c8834efb227aa7df76ae743f7397fe
badlogic <badlogicgames@gmail.com>
2015-12-25 12:30:32 +0100

    Initial commit

---
+++ html/src/com/badlogicgames/spacedorks/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.badlogicgames.spacedorks.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogicgames.spacedorks.SpaceDorks;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceDorks();
+        }
+}
